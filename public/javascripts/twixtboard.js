be7cd7f9024aebc1870d6a7f0409030b8bd976e2
Alan Hensel <alhensel@gmail.com>
2009-03-20 00:30:38 -0400

    ended the javascript without semicolons experiment

--- public/javascripts/twixtboard.js
+++ public/javascripts/twixtboard.js
@@ -1,37 +1,37 @@
 function TwixtBoard(s)
 {
-  this.size = s
+  this.size = s;
   
-  this.board = new Array()
+  this.board = [];
   for (var i=1; i <= this.size; i++) {
-    this.board[i] = new Array()
+    this.board[i] = [];
   }
   
   this.isLegalSpot = function(x, y, color) {
     return ((color == 1 && x > 1 && x < this.size && y >=1 && y <=this.size) ||
-            (color == 0 && x >=1 && x <=this.size && y > 1 && y < this.size))
-  }
+            (color == 0 && x >=1 && x <=this.size && y > 1 && y < this.size));
+  };
   
   this.setPeg = function(peg) {
     if (this.board[peg.x][peg.y] == null) {
-      this.board[peg.x][peg.y] = peg
+      this.board[peg.x][peg.y] = peg;
     }
-  }
+  };
   
   this.getPeg = function(x, y) {
-    if (x < 1 || x > this.size || y < 1 || y > this.size) return null
-    if (this.board[x] == null) return null
-    return this.board[x][y]
-  }
+    if (x < 1 || x > this.size || y < 1 || y > this.size) return null;
+    if (this.board[x] == null) return null;
+    return this.board[x][y];
+  };
   
   this.getAllPegs = function() {
-    var allPegs = new Array()
+    var allPegs = [];
     for (var y = 1; y <= this.size; y++) {
       for (var x = 1; x <= this.size; x++) {
-        var peg = this.board[x][y]
-        if (peg) allPegs.push(peg)
+        var peg = this.board[x][y];
+        if (peg) allPegs.push(peg);
       }
     }
-    return allPegs
-  }
+    return allPegs;
+  };
 }
