a5022ef6ff8df3bf09e685a0438d13f7d63d6f90
Alan Hensel <alhensel@gmail.com>
2009-03-27 11:52:15 -0400

    capistrano deployment

---
+++ config/deploy.rb
@@ -0,0 +1,26 @@
+
+#	Application
+set :application, "twixt-commentator"
+set :deploy_to, "/Library/Rails/#{application}"
+
+#	Settings
+default_run_options[:pty] = true
+set :use_sudo, true
+
+#	Servers
+set :domain, "zippy.local"
+server domain, :app, :web, :db, :primary => true
+
+#	Source Control
+set :scm, :git
+set :repository,  "ssh://zippy.local/var/git/twixt-commentator.git"
+
+#	Passenger
+namespace :passenger do
+  desc "Restart Application"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
+after :deploy, "passenger:restart"
