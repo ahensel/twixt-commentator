8711b3c703cc74ca93cbc5d2eed0ab8593d8e342
Alan Hensel <alhensel@gmail.com>
2009-06-21 17:57:25 -0400

    fixed bug downloading jtwixt files for in-progress games

--- app/controllers/jtwixt_controller.rb
+++ app/controllers/jtwixt_controller.rb
@@ -3,15 +3,45 @@ require 'LittleGolemParser'
 
 class JtwixtController < ApplicationController
   def gen
-    game = Game.find(:first, :conditions => ["lg_game_num = ?", params[:gameid]])
+    game_id = params[:gameid]
+    game = Game.find(:first, :conditions => ["lg_game_num = ?", game_id]) || getGameFromLittleGolem(game_id)
+
+    if (game)
+      file_data = jTwixtFileData(game)
+      file_name = "game#{game_id}.tgt"
     
-    file_data = jTwixtFileData(game)
-    file_name = "game#{params[:gameid]}.tgt"
-    
-    send_data(file_data, :filename => file_name, :type => "application/octet-stream")
+      send_data(file_data, :filename => file_name, :type => "application/octet-stream")
+    end
   end
 
 private
+  #refactor: this is remarkably similar to code in game_controller...
+  def getGameFromLittleGolem(game_number)
+    http_connection = Net::HTTP::new('www.littlegolem.net')
+    lg_response, lg_data = http_connection.get("/jsp/game/png.jsp?gid=#{game_number}&#{rand.to_s}")
+    
+    if lg_response.code != '200'
+      return nil  # HTML error
+    else
+      lg_data = lg_data.strip
+
+      if lg_data.index('SZ[24]') == nil
+        return nil  # not a Twixt game
+      end
+      
+      parser = LittleGolemParser.new(lg_data)
+      
+      return Game.new(
+        :lg_game_num => game_number,
+        :lg_data => lg_data,
+        :result => '?',
+        :player1 => parser.getPlayer1,
+        :player2 => parser.getPlayer2,
+        :winner => 0,
+        :tournament => parser.getTournament
+      )
+    end
+  end
   
   def jTwixtFileData(game)
     parser = LittleGolemParser.new(game.lg_data)
