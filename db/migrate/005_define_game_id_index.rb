b2bda8f3433def1406ab0072d71783bf80140bf6
Alan Hensel <alhensel@gmail.com>
2026-04-20 19:36:39 -0400

    Removed Ruby on Rails code

--- db/migrate/005_define_game_id_index.rb
+++
@@ -1,9 +0,0 @@
-class DefineGameIdIndex < ActiveRecord::Migration
-  def self.up
-    add_index :games, :lg_game_num, :unique => true
-  end
-
-  def self.down
-    remove_index :games, :lg_game_num
-  end
-end
--- db/migrate/006_main_page_columns_for_games.rb
+++
@@ -1,9 +0,0 @@
-class MainPageColumnsForGames < ActiveRecord::Migration
-  def self.up
-    add_column :games, :last_commented_on, :datetime
-  end
-
-  def self.down
-    remove_column :games, :last_commented_on
-  end
-end
